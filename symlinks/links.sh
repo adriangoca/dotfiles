c29a460bb24e35223d6b61fb1af2d3a316d4d9c8
Adrian Gonzalez <adrian.gonzalez@freshlycosmetics.com>
2021-07-28 00:54:31 +0200

    Create folder structure

---
+++ symlinks/links.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+ln -sf "$HOME/.dotfiles/shell/.bashrc" "$HOME/.bashrc"
+ln -sf "$HOME/.dotfiles/shell/.bash_aliases" "$HOME/.bash_aliases"
+ln -sf "$HOME/.dotfiles/shell/.profile" "$HOME/.profile"
+ln -sf "$HOME/.dotfiles/shell/.tmux.conf" "$HOME/.tmux.conf"
+ln -sf "$HOME/.dotfiles/git/.gitconfig" "$HOME/.gitconfig"
