5263de562bac0aab14a0885867084eb5300fc92b
Adrian Gonzalez <adrian.gonzalez@freshlycosmetics.com>
2021-08-01 01:50:01 +0200

    Create my first navi cheat

--- symlinks/links.sh
+++ symlinks/links.sh
@@ -5,3 +5,6 @@ ln -sf "$HOME/.dotfiles/shell/.bash_aliases" "$HOME/.bash_aliases"
 ln -sf "$HOME/.dotfiles/shell/.profile" "$HOME/.profile"
 ln -sf "$HOME/.dotfiles/shell/.tmux.conf" "$HOME/.tmux.conf"
 ln -sf "$HOME/.dotfiles/git/.gitconfig" "$HOME/.gitconfig"
+
+# navi cheats
+ln -sf "$HOME/.dotfiles/doc/navi" "$HOME/.local/share/navi/cheats"
