49f59fc9344de8292b1f63ec9d623ccba104354c
Adrian Gonzalez <adrian.gonzalez@freshlycosmetics.com>
2021-08-03 23:41:53 +0200

    Add function to import apps

--- shell/functions.sh
+++ shell/functions.sh
@@ -5,3 +5,8 @@ function export_apps() {
   brew bundle dump --file="$OS_LINUX_PATH/brew/brewfile" --force
   echo 'brew apps exported!'
 }
+
+function import_apps() {
+  sudo xargs -a "$OS_LINUX_PATH/apt/packages_list.txt" apt install
+  brew bundle --file="$OS_LINUX_PATH/brew/brewfile" --force
+}
